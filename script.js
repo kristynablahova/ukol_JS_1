0e995171f2c5ffcef2561176fc1c307b8c843768
Kristýna Bláhová <kristynablahova1234@gmail.com>
2024-10-23 10:20:38 +0200

    prvni_pokus

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ Pokud se přihlásíte jako student například na Fakultu Informačních Techno
 
 Vytvořte webovou stránku, která požádá uživatele nejdříve o jeho křestní jméno a pak příjmení, obojí bez diakritiky. Pomocí metody trim zajistěte, že ve jméně ani příjmení nebudou na začátku ani na konci žádné bílé znaky. Pomocí metody slice vyřízněte ze jména i příjmení příslušné části. Pomocí interpolace řetězců sestavte výslednou adresu a vypište ji do stránky.
 */
+
 const jmeno = prompt("Zadejte Vaše křestní jméno (bez diakritiky):").trim()
 const prijmeni = prompt("Zadejte Vaše příjmení (bez diakritiky):").trim()
 
+console.log(jmeno)
+console.log(prijmeni)
 
-const castJmena = jmeno.slice(0, 2)
-const castPrijmeni = prijmeni.slice(0, 4)
+const jmenoCast = jmeno.slice(0, 2).toLocaleLowerCase()
+const prijmeniCast = prijmeni.slice(0, 4).toLocaleLowerCase()
+
+console.log(jmenoCast)
+console.log(prijmeniCast)
+
 const domena = "@fit.cvut.cz"
 
-document.body.innerHTML = 
-    <p>$castJmena + $castPrijmeni + $domena</p>
+const zprava = `
+    <p>
+    Tvůj email je: ${prijmeniCast}${jmenoCast}${domena}
+    </p>
+    `
 
+document.body.innerHTML = zprava
 
